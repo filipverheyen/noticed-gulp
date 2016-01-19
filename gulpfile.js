9002a735b495291d70f283896f1b9a56ea71c0a6
MacBook Pro <macbookpro@MacBook-Pro.local>
2016-01-19 10:20:42 +0100

    sourcemaps and error handling support added

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,28 @@ var sass = require('gulp-sass');
 var cssGlobbing = require('gulp-css-globbing');
 var livereload = require('gulp-livereload');
 
+function errorAlert(error){
+	notify.onError({title: "SCSS Error", message: "Check your terminal", sound: "Sosumi"})(error); //Error Notification
+	console.log(error.toString());//Prints Error to Console
+	this.emit("end"); //End function
+};
+
 gulp.task('sass', function(){
   	return gulp.src('assets/scss/**/*.scss')
 	  	.pipe(cssGlobbing({
 	      // Configure it to use SCSS files
 	      extensions: ['.scss']
 	    }))
+	.pipe(plumber({errorHandler: errorAlert}))
     	.pipe(sass()) // Using gulp-sass
     	.pipe(gulp.dest('assets/css'))
     	.pipe(livereload())
+    	.pipe(sourcemaps.init())
+    	.pipe(sourcemaps.write('./'));
 });
 
 gulp.task('watch', function(){
 	livereload.listen();
-  	gulp.watch('assets/scss/**/*.scss', ['sass']); 
+  	gulp.watch('assets/scss/**/*.scss', ['sass']);
   	// Other watchers
 });
